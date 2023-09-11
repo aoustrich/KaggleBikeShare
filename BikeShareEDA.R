7d50c7b88ff6a3ebf7a8d3fdba3c434b78fcf5f4
Aaron Oustrich <aaron.oustrich@gmail.com>
2023-09-11 13:50:34 -0600

    EDA

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -5,6 +5,23 @@
 # Libraries
 library(tidyverse)
 library(vroom)
+library(DataExplorer)
 
 # read in data
 bike <-  vroom("./train.csv")
+
+# change factor columns
+bike$season <- as.factor(bike$season)
+bike$weather <- as.factor(bike$weather)
+
+# EDA
+dplyr::glimpse(bike)
+
+skimr::skim(bike)
+
+plot_intro(bike)
+DataExplorer::plot_correlation(bike)
+DataExplorer::plot_bar(bike)
+DataExplorer::plot_histrograms(bike)
+DataExplorer::plot_missing(bike)
+GGally::ggpairs(bike)
