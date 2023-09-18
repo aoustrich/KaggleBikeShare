f508400581131dcc0892ec3252d0434838f3cc82
Aaron Oustrich <aaron.oustrich@gmail.com>
2023-09-18 13:33:21 -0600

    Add Poisson Regression, edit recipe

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(tidymodels)
 library(vroom)
+library(poissonreg)
 
 # read in data
 bike.train <-  vroom("./train.csv")
@@ -17,33 +18,52 @@ bike.train <- bike.train %>%
 
 ## Data Expansion
 
-my_recipe <- recipe(count ~ . , data = bike.train) %>%  # Set formula and dateset
-  # mutate(weather = ifelse(weather == 4, 3, weather)) %>% 
-  # step_mutate(weatherF = factor(weather, #make factor of weather
-  #                               levels = c(1,2,3),
-  #                               labels=c("Sunny","Light Precip","Heavy Precip"))) %>%
-  step_dummy(all_nominal_predictors()) %>%  # create dummy variables
-  step_zv(all_predictors())  # remove near-zero variance variables
+# my_recipe <- recipe(count ~ . , data = bike.train) %>%  # Set formula and dateset
+#   # mutate(weather = ifelse(weather == 4, 3, weather)) %>% 
+#   # step_mutate(weatherF = factor(weather, #make factor of weather
+#   #                               levels = c(1,2,3),
+#   #                               labels=c("Sunny","Light Precip","Heavy Precip"))) %>%
+#   step_dummy(all_nominal_predictors()) %>%  # create dummy variables
+#   step_zv(all_predictors())  # remove near-zero variance variables
+
+my_recipe <- recipe(count~., data=bike.train) %>%
+  step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Relabel weather 4 to 3
+  step_mutate(weather=factor(weather, levels=1:3, labels=c("Sunny", "Mist", "Rain"))) %>%
+  step_mutate(season=factor(season, levels=1:4, labels=c("Spring", "Summer", "Fall", "Winter"))) %>%
+  step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_time(datetime, features="hour") %>%
+  step_rm(datetime) %>% 
+  step_zv(all_predictors())
 
 
 prepped_recipe <- prep(my_recipe)
 bake(prepped_recipe, new_data = bike.train)
 
-## Model Fitting
+# Linear Regression Model Fitting ----------------------
+# Define model
 model <- linear_reg() %>% #Type of model
   set_engine("lm") 
 
+# set up workflow
 bike_workflow <- workflow() %>% 
   add_recipe(my_recipe) %>% 
   add_model(model) %>% 
   fit(data = bike.train)
+
+# look at the fitted model
+extract_fit_engine(bike_workflow) %>% 
+  tidy()
+extract_fit_engine(bike_workflow) %>% 
+  summary()
+
 
 # make predictions
 bike_preds <- predict(bike_workflow, new_data = bike.test)
 
 # remove negative predictions
 bike_preds <- bike_preds %>% 
-  mutate(.pred = ifelse(.pred < 0 , 0, .pred)) %>% 
+  mutate(.pred = ifelse(.pred < 1 , 1, .pred)) %>% #
   mutate(.pred = round(.pred, digits = 3))
 
 # prep data for submission
@@ -54,3 +74,38 @@ out <- cbind(bike.test$datetime, bike_preds) %>%
 
 # write to .csv
 vroom_write(out, file="BikeSubmission.csv",delim=',')
+
+# Poisson Regression ------------------------------------------------------
+# define model
+pois_mod <- poisson_reg() %>%
+  set_engine("glm") 
+
+# run workflow
+bike_pois_workflow <- workflow() %>%
+add_recipe(my_recipe) %>%
+add_model(pois_mod) %>%
+fit(data = bike.train) 
+
+# get predictions and prep for kaggle
+bike_predictions <- predict(bike_pois_workflow,new_data=bike.test)  %>%
+  bind_cols(., bike.test) %>% 
+  select(datetime, .pred) %>% 
+  rename(count=.pred) %>% 
+  mutate(count=pmax(1, count)) %>% 
+  mutate(datetime=as.character(format(datetime))) 
+
+# look at the fitted model
+extract_fit_engine(bike_pois_workflow) %>% 
+  tidy()
+
+# write out data
+vroom_write(bike_predictions, file="BikeSubmissionPois.csv",delim=',')
+
+
+
+
+
+
+
+
+
